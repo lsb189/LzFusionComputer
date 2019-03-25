7fa81a10ac25b8e0bfb76ead53825c7358b48460
lusb <lusb@LUSB-PC>
2019-03-26 06:34:13 +0800

    第一次提交

---
+++ FusionComputerCore/FusionComputerCore/Test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FusionComputerCore
+{
+    public class Test
+    {
+        public string Name { get; set; }
+    }
+}
