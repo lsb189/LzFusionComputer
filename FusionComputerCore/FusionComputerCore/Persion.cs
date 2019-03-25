8d65bafcafd86e276a398aa004365a84872c2247
lusb <lusb@LUSB-PC>
2019-03-26 06:50:01 +0800

    Persion Class

---
+++ FusionComputerCore/FusionComputerCore/Persion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FusionComputerCore
+{
+    class Persion
+    {
+        public string Name { get; set; }
+        public string Adress { get; set; }
+    }
+}
